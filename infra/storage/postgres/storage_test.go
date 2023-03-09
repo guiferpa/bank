af67bd4fb82ce54564af14af0eb69afbd0cc8710
Guilherme Paixão <guiferpa@gmail.com>
2023-03-09 09:43:26 -0300

    test: created test to create account transaction

--- infra/storage/postgres/storage_test.go
+++ infra/storage/postgres/storage_test.go
@@ -272,3 +272,87 @@ func TestGetAccountByID(t *testing.T) {
 		}
 	}()
 }
+
+func TestCreateTransaction(t *testing.T) {
+	env, err := infratest.NewEnvironment()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx := context.Background()
+
+	containerID, err := env.RunContainer(ctx, "postgres:14", "5432", []string{
+		"POSTGRES_PASSWORD=Pa$$w0rd",
+		"POSTGRES_DB=infra",
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	newStorageOptions := NewStorageOptions{
+		Host:         "localhost",
+		User:         "postgres",
+		Password:     "Pa$$w0rd",
+		DatabaseName: "infra",
+		Port:         "5432",
+	}
+	client, err := NewStorage(newStorageOptions)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	suite := []struct {
+		Describe string
+		Spec     func(t *testing.T)
+	}{
+		{
+			Describe: "Created successful",
+			Spec: func(t *testing.T) {
+				acc := &Account{DocumentNumber: "42"}
+				if err := client.db.Create(&acc).Error; err != nil {
+					t.Error(err)
+					return
+				}
+
+				transOptions := account.CreateTransactionOptions{
+					AccountID:       acc.ID,
+					OperationTypeID: 1, // COMPRA A VISTA
+					Amount:          -10_00,
+					EventDate:       time.Now(),
+				}
+				transID, err := client.CreateTransaction(transOptions)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				var dest AccountTransaction
+				if err := client.db.Select("*").Where("id = ?", transID).First(&dest).Error; err != nil {
+					t.Error(err)
+					return
+				}
+
+				if got, expected := dest.Amount, transOptions.Amount; got != expected {
+					t.Errorf("unexpected transaction's Amount, got: %v, expected: %v", got, expected)
+					return
+				}
+			},
+		},
+	}
+
+	for _, s := range suite {
+		t.Run(s.Describe, s.Spec)
+	}
+
+	defer func() {
+		if err := env.KillContainer(ctx, containerID); err != nil {
+			t.Error(err)
+			return
+		}
+	}()
+}
