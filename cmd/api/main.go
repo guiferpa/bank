61848f01e99ac69e65972de93f95af733c2a40a1
Guilherme Paixão <guiferpa@gmail.com>
2023-03-10 14:44:18 -0300

    feat: implemented entrypoint composing all layers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github/guiferpa/bank/domain/account"
+	"github/guiferpa/bank/handler/http/rest"
+	"github/guiferpa/bank/infra/storage/postgres"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world API!!!")
+	storage, err := postgres.NewStorage(postgres.NewStorageOptions{
+		Host:         os.Getenv("DATABASE_HOST"),
+		User:         os.Getenv("DATABASE_USER"),
+		Password:     os.Getenv("DATABASE_PASSWORD"),
+		DatabaseName: os.Getenv("DATABASE_NAME"),
+		Port:         os.Getenv("DATABASE_PORT"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	service := account.NewUseCaseService(storage)
+	handler := rest.NewHTTPHandler(service)
+
+	port := os.Getenv("PORT")
+	http.ListenAndServe(fmt.Sprintf(":%s", port), handler)
 }
